151cf0c2ffa0cca25f849d6fab507cbe8138a911
FelixvL <digitaleservice@gmail.com>
2017-10-05 13:57:10 +0200

    Start BankApplicatie - Voor import Eclipse

---
+++ src/main/java/com/itph/BankApplicatie/BankApplicatieApplication.java
@@ -0,0 +1,12 @@
+package com.itph.BankApplicatie;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BankApplicatieApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BankApplicatieApplication.class, args);
+	}
+}
